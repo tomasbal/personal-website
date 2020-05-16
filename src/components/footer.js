6919c919ebca5ed8fb34735adfadb06e3c14373e
Tomislav Balabanov <tomislav.balabanov@students.finki.ukim.mk>
2020-05-16 23:46:13 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -61,26 +61,6 @@ const StyledGitHubInfo = styled.div`
 `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
 
   return (
     <StyledContainer>
